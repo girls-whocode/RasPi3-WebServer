6bd8adeba6cb68d5e5c3040070a440de3ae6ebed
Jessica Brown <jessica@jbrowns.com>
2017-11-17 19:38:54 -0500

    Resolved menu back, ESC menu flow Commited the menu better to show each option

--- test.sh
+++ test.sh
@@ -6,3 +6,7 @@ switchstatus=$([ "$status" == "true" ] && echo "TRUE" || ([ "$status" == "false"
 
 echo "Your choice is ${switchstatus} and I did not execute anything else"
 
+phymem=$(awk '/^MemTotal:/{print $2}' /proc/meminfo)
+swpmem=$(awk '/^SwapTotal:/{print $2}' /proc/meminfo)
+echo $swpmem
+echo $phymem
